fb0d17120fdb7b23977bf60d3a69a43885e5dd4f
prsaha <pr.saha@f5.com>
2022-04-17 22:38:32 +0530

    Day 3 playing CSS Variables

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const handleUpdate = function()
+{
+    console.log(this.value);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+const inputs = document.querySelectorAll(".controls input");
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
